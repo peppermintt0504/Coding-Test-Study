50b1abcddd03d65a32575a952d576850d39511c7
peppermintt0504 <73277351+peppermintt0504@users.noreply.github.com>
2022-02-12 02:43:58 +0900

    Feb 12, 2022

---
+++ inflearn/완전_탐색/K번째 큰 수.js
@@ -0,0 +1,29 @@
+function solution(N,K,list) {
+    
+    let sorted_list = list.sort((a,b) => a-b);
+    let sum_list = [];
+    
+    for(let i = 0; i < N; i++){
+
+        for(let j = i; j < N; j++){
+            if(j === i){
+                continue;
+            }
+
+            for(let r = j; r < N; r++){
+                if(r === i || r === j){
+                    continue;
+                }
+                sum_list.push(list[i] + list[j] + list[r]);
+            }
+        }
+    }
+    return sum_list.sort((a,b) => -a+b)[2];
+
+}
+
+const N = 10;
+const K = 3;
+const list = [13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
+
+console.log(solution(N,K,list));
