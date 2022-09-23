c09ac2ac7e2a6a3f5a010f047e70fce31be5e52f
peppermintt0504 <73277351+peppermintt0504@users.noreply.github.com>
2022-09-23 21:33:13 +0900

    add gitignore

--- beakjoon/note.js
+++ beakjoon/note.js
@@ -15,37 +15,6 @@ function solution(){
     let answer = 0;
     let [N , M] = (input().split(" ").map((v)=>+v));
 
-    let A = [];
-    let B = [];
-
-    const reverseBoard = (a,b) =>{
-        let tempBoard = [].concat(A);
-        for(let i = 0; i < 3;i++){
-            for(let j = 0; j < 3;j++){
-                tempBoard[a+i][b+j] = tempBoard[a+i][b+j] === 1 ? 0 : 1; 
-            }
-        }
-    }
-
-
-    for(let i = 0; i < N; i++){
-        A.push(input().split('').map((v)=>+v));
-    }
-    for(let i = 0; i < N; i++){
-        B.push(input().split('').map((v)=>+v));
-    }
-    
-    for(let i = 0; i < N - 2; i++){
-        for(let j = 0; j < M - 2; j++){
-            if(A[i][j] !== B[i][j]){
-                reverseBoard(i,j);
-                answer += 1;
-            }
-        }
-    }
-    console.log(JSON.stringify(A) === JSON.stringify(B) ? answer : -1);
-    // console.table(A);
-    // console.table(B);
 }
 
 solution(); 
