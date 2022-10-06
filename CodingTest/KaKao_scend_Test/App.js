1605787c9ef3cdeb33b9bc423649316a5af06fc1
dongkun yoo <73277351+peppermintt0504@users.noreply.github.com>
2022-10-06 20:47:54 +0900

    test

--- CodingTest/KaKao_scend_Test/App.js
+++ CodingTest/KaKao_scend_Test/App.js
@@ -1,31 +1,71 @@
+import axios from "axios"
+
 async function post(host, path, body, headers = {}) {
     const url = `https://${host}/${path}`;
-    const options = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            ...headers,
-        },
-        body: JSON.stringify(body),
-    };
-    const res = await fetch(url, options);
-    const data = await res.json();
-    if (res.ok) {
-        console.log(data);
-        return data;
-    } else {
-        throw Error(data);
-    }
+    const res = await axios.post(
+        url,
+        body,
+        {
+            headers: {
+                "Content-Type": "application/json",
+                ...headers,
+            },
+        }
+      );
+    const data = res.data;
+    return data;
+}
+
+async function get(host, path, headers = {}) {
+    const url = `https://${host}/${path}`;
+
+    const res = await axios.get(
+        url,
+        {
+            headers: {
+                "Content-Type": "application/json",
+                ...headers,
+            },
+        }
+      );
+    const data = res.data;
+    return data;
+}
+
+async function put(host, path, body, headers = {}) {
+    const url = `https://${host}/${path}`;
+    const res = await axios.put(
+        url,
+        body,
+        {
+            headers: {
+                "Content-Type": "application/json",
+                ...headers,
+            },
+        }
+      );
+    const data = res.data;
+    return data;
 }
 
 const BaseUrl = 'huqeyhi95c.execute-api.ap-northeast-2.amazonaws.com/prod';
 const token = 'e12cea7b63f37977eb3d9ab5d77dbe07';
 
 const test = async ()=>{
+    const startData = await post(BaseUrl,'start',{'problem' : 1},{'X-Auth-Token':token})
+    const startKey = {Authorization : startData.auth_key};
 
-    const data = post(BaseUrl,'start',{'problem' : 1},{'X-Auth-Token':token})
+    const userInfo = await get(BaseUrl,'user_info',startKey)
+    let match = await put(BaseUrl,'match',{'pairs' : []},startKey)
+    const waitingList = await get(BaseUrl,'waiting_line',startKey)
+    let gameResult = await get(BaseUrl,'game_result',startKey)
 
-    console.log(data);
+
+    console.log(startKey);
+    console.log(waitingList);
+    // console.log(userInfo);
+    console.log(match);
+    
 }
 
 test();
