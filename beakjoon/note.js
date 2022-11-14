9fc9cc643669b3840a534cc74f33dff2984c7207
peppermintt0504 <73277351+peppermintt0504@users.noreply.github.com>
2022-11-14 22:02:15 +0900

    Update note.js

--- beakjoon/note.js
+++ beakjoon/note.js
@@ -8,22 +8,3 @@ const input = (() => {
     return () => stdin[line++];
 })();
 
-const n = Number(input());
-const numList = input().split(' ').map(Number);
-let answer = Array(100010 * n).fill(0);
-let search = 1;
-
-for(let i = 1; i < Math.pow(2,n);i++){
-    let binary = i.toString(2).split('').map(Number);
-    if(binary.length !== n)   binary = Array(n - binary.length).fill(0).concat(binary)
-    let sum = binary.reduce((x,v,j)=> (v * numList[n - 1 - j]) + x,0)
-    answer[sum]= 1 ;
-}
-
-while(true){
-    if(!answer[search]){
-        console.log(search);
-        break;
-    }
-    search++;
-}
