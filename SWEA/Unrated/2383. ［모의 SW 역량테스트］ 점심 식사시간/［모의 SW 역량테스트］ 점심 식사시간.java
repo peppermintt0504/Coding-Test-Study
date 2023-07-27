5c6b2ee95c333a35653d19ee8b931fee43b80581
Yooooodong <73277351+peppermintt0504@users.noreply.github.com>
2023-07-27 17:46:32 +0900

    [Unrated] Title: [모의 SW 역량테스트] 점심 식사시간, Time: 249 ms, Memory: 33,656 KB -BaekjoonHub

--- SWEA/Unrated/2383. ［모의 SW 역량테스트］ 점심 식사시간/［모의 SW 역량테스트］ 점심 식사시간.java
+++ SWEA/Unrated/2383. ［모의 SW 역량테스트］ 점심 식사시간/［모의 SW 역량테스트］ 점심 식사시간.java
@@ -3,12 +3,11 @@ import java.util.*;
 
 class Solution
 {
-	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 	public static ArrayList<int[]> p;
 	public static ArrayList<int[]> step;
-	public static ArrayList<Person[]> p2 = new ArrayList<>();
 	
 	public static int step1Length;
 	public static int step2Length;
@@ -20,9 +19,7 @@ class Solution
 		int y;
 		int distance;
 		
-		public Person() {
-			// TODO Auto-generated constructor stub
-		}
+		public Person() {}
 
 		public Person(int x, int y, int distance) {
 			super();
@@ -33,7 +30,6 @@ class Solution
 
 		@Override
 		public int compareTo(Person o) {
-			
 			return this.distance - o.distance;
 		}
 
@@ -41,7 +37,6 @@ class Solution
 		public String toString() {
 			return "Person [x=" + x + ", y=" + y + ", distance=" + distance + "]";
 		}
-		
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -49,10 +44,13 @@ class Solution
 		int T = Integer.parseInt(br.readLine());
 		
 		for(int t = 1; t <= T; t++) {
+			
 			int N = Integer.parseInt(br.readLine());
+			
 			answer= 10000;
 			p = new ArrayList<>();
 			step = new ArrayList<>();
+			
 			for(int i = 0; i < N; i++) {
 				int[] temp = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 				for(int x = 0; x < N; x++) {
@@ -69,15 +67,13 @@ class Solution
 			step1Length = step.get(0)[2];
 			step2Length = step.get(1)[2];
 			
-			
-
 
 			boolean[] visited = new boolean[p.size()];
 			Person[] array = new Person[p.size()];
 			
 			for(int i = 0; i < p.size();i++) {
 				int[] tempP = p.get(i);
-				array[i] =(new Person(tempP[0],tempP[1],Math.abs(step.get(0)[0] - tempP[0]) + Math.abs(step.get(0)[1] - tempP[1])));
+				array[i] =(new Person(tempP[0],tempP[1],0));
 			}
 			
 			powerSet(array,visited,p.size(),0);
@@ -89,29 +85,23 @@ class Solution
 		bw.close();
 	}
 	
-	public static int checkTime (Queue<Person> s1, Queue<Person> s2) throws IOException {
-		int count = 0;
-		
+	public static void checkTime (Queue<Person> s1, Queue<Person> s2) throws IOException {
 		int s1Count = 0;
 		int s2Count = 0;
-		
+
 		int[] time = new int[100];
-		
-		
+			
 		while(!s1.isEmpty()) {
 			Person target = s1.poll();
 			int start = target.distance + 1;
 			int d = 0;
 			
-			while(time[start + d] >= 3) {
-				d++;
-			}
+			while(time[start + d] >= 3) d++;
 			
 			for(int t = 0; t < step1Length; t++) {
 				time[t + start + d]++;
 				if(t == step1Length-1) s1Count = t + start + d;
 			}
-			
 		}
 		
 		time = new int[100];
@@ -120,20 +110,15 @@ class Solution
 			int start = target.distance + 1;
 			int d = 0;
 			
-			while(time[start + d] >= 3) {
-				d++;
-			}
+			while(time[start + d] >= 3) d++;
 			
 			for(int t = 0; t < step2Length; t++) {
 				time[t + start + d]++;
 				if(t == step2Length-1) s2Count = t + start + d;
 			}
-			
 		}
 		
 		if(Math.max(s1Count, s2Count) < answer) answer = Math.max(s1Count, s2Count);
-		
-		return count;
 	}
 	
 	static void powerSet(Person[] arr, boolean[] visited, int n, int idx) throws IOException {
@@ -154,9 +139,6 @@ class Solution
 		PriorityQueue<Person> queue2 = new PriorityQueue<>();
         for (int i = 0; i < n; i++) {
             if (visited[i] == true)
-            	
-            	
-            	
                 queue1.add(new Person(arr[i].x,arr[i].y,Math.abs(step.get(0)[0] - arr[i].x) + Math.abs(step.get(0)[1] - arr[i].y)));
             else
             	queue2.add(new Person(arr[i].x,arr[i].y,Math.abs(step.get(1)[0] - arr[i].x) + Math.abs(step.get(1)[1] - arr[i].y)));
