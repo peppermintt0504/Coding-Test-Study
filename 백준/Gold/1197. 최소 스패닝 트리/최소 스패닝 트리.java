f4e576dfcf9bb2ab48970b4faace1970b9aff0c3
Yooooodong <73277351+peppermintt0504@users.noreply.github.com>
2023-08-09 17:06:24 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 572 ms, Memory: 47808 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -10,17 +10,19 @@ public class Main {
 
 	public static int V;
 	public static int E;
+	public static int[] parent;
 	public static Map<Integer,ArrayList<Node>> nodes = new HashMap<>();
 	public static PriorityQueue<Node> pq = new PriorityQueue<>();
 	
 	public static class Node implements Comparable<Node>{
-
+		int from;
 		int to;
 		int val;
 		
 		public Node() {}
 
-		public Node(int to,int val) {
+		public Node(int from,int to,int val) {
+			this.from = from;
 			this.to = to;
 			this.val = val;
 		}
@@ -37,42 +39,56 @@ public class Main {
 		V = Integer.parseInt(st.nextToken());
 		E = Integer.parseInt(st.nextToken());
 		
+		parent = new int[V+1];
+		for(int i=1; i <= V; i++) {
+			parent[i] = i;
+		}
+		
 		while(E --> 0) {
 			st = new StringTokenizer(br.readLine());
 			int n1 = Integer.parseInt(st.nextToken());
 			int n2 = Integer.parseInt(st.nextToken());
 			int val = Integer.parseInt(st.nextToken());
 			
-			
-			
-			if(!nodes.containsKey(n1)) 
-				nodes.put(n1, new ArrayList<>());
-			nodes.get(n1).add(new Node(n2,val));
-			
-			if(!nodes.containsKey(n2)) 
-				nodes.put(n2, new ArrayList<>());
-			nodes.get(n2).add(new Node(n1,val));
-			
-
+			pq.add(new Node(n1,n2,val));
 		}
 		
 		boolean[] visited = new boolean[V+1];
 		int answer = 0;
 		
-		pq.add(new Node(1,0));
-		
 		while(!pq.isEmpty()) {
 			Node cur = pq.poll();
 			
-			if(visited[cur.to]) continue;
-			visited[cur.to] = true;
-			answer+=cur.val;
 			
-			for(Node n : nodes.get(cur.to)) {
-				if(!visited[n.to])
-					pq.add(n);
+			int to = find(cur.to);
+			int from = find(cur.from);
+			
+			if(!isSameParent(to,from)) {
+				answer += cur.val;
+				union(cur.to, cur.from);
 			}
+			
 		}
 		System.out.println(answer);
+	}
+	
+	public static int find(int x) {
+		if(parent[x] ==x ) return x;
+		return parent[x] = find(parent[x]);
+	}
+	
+	public static void union(int x, int y) {
+		x = find(x);
+		y = find(y);
+		if(x!= y) {
+			parent[y] =x;
+		}
+	}
+	
+	public static boolean isSameParent(int x, int y ) {
+		x = find(x);
+		y = find(y);
+		if(x==y) return true;
+		else return false;
 	}
 }
