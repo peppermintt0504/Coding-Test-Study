c6a8117a0a24c98c2fddf7f582f3db079504d778
Yooooodong <animus0504@naver.com>
2023-08-17 14:42:16 +0900

    [D4] Title: [S/W 문제해결 기본] 6일차 - 계산기1, Time: 184 ms, Memory: 27,452 KB -BaekjoonHub

---
+++ SWEA/D4/1222. ［S／W 문제해결 기본］ 6일차 － 계산기1/README.md
@@ -0,0 +1,11 @@
+# [D4] [S/W 문제해결 기본] 6일차 - 계산기1 - 1222 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14mbSaAEwCFAYD) 
+
+### 성능 요약
+
+메모리: 27,452 KB, 시간: 184 ms, 코드길이: 919 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/1222. ［S／W 문제해결 기본］ 6일차 － 계산기1/［S／W 문제해결 기본］ 6일차 － 계산기1.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.util.stream.*;
+import java.io.*;
+
+public class Solution {
+	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static StringTokenizer st;
+	public static StringBuilder sb = new StringBuilder();
+	public static int INF = 100_000_000;
+	public static int[] dx = {0,1,0,-1};
+	public static int[] dy = {-1,0,1,0};
+
+	
+	public static void main(String[] args) throws IOException{
+//		int T = Integer.parseInt(br.readLine());
+		int T = 10;
+		
+		for(int t = 1; t <= T; t++) {
+			int N = Integer.parseInt(br.readLine());
+			int[] nums = Arrays.stream(br.readLine().split("\\+")).mapToInt(Integer::parseInt).toArray();
+			
+			int sum = Arrays.stream(nums).reduce(0, (a,b)->a+b);
+			
+			System.out.printf("#%d %d",t,sum);
+			if(t <T)System.out.println();
+		}
+		
+	}
+}
