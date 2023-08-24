572b5375b327274d18985bf32f81fac84f272e4b
Yooooodong <animus0504@naver.com>
2023-08-24 11:45:40 +0900

    [D3] Title: 농작물 수확하기, Time: 251 ms, Memory: 39,200 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -1,33 +1,57 @@
-import java.io.*;
 import java.util.*;
+import java.util.stream.*;
+import java.io.*;
 
-class Solution
-{
-    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    
-	public static void main(String args[]) throws Exception
-	{
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int t = 0; t < T; t++) {
-			int N = Integer.parseInt(br.readLine());
-			int answer = 0;
-			int[][] board = new int[N][N];
-			
-			for(int i = 0; i < N; i++) 
-				board[i] = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
-			
-			for(int i = 0 ; i < N; i++) {
-				for(int z = Math.abs((int) (N / 2) - i); z < N - Math.abs((int) (N / 2) - i); z++) {
-					answer += board[i][z];
-				}
-			}
-			System.out.printf("#%d %d",t+1,answer);
-			if(t != T - 1)System.out.println();
+public class Solution {
+	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static StringTokenizer st;
+
+	public static class Node{
+		int val;
+		Node left;
+		Node right;
+		Node parent;
+		public Node() {}
+		public Node(int val) {
+			this.val = val;
 		}
 		
-		
+		public void setVal(int val) {
+			this.val = val;
+		}
+
+		public void addChild(Node node) {
+			if(this.left == null) this.left = node;
+			else				  this.right = node;
+		}
+		public void setParent(Node node) {
+			this.parent = node;
+		}
 	}
+	
+	public static void main(String[] args) throws IOException{
+		int T = Integer.parseInt(br.readLine());
+
+		for(int t = 1 ; t <= T; t++) {
+			
+			
+			int N = Integer.parseInt(br.readLine());
+	
+			int[][] board = new int[N][N];
+			
+			for(int i = 0 ; i < N; i++) {
+				board[i] = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
+			}
+			int sum = 0;
+			
+			for(int i = 0; i < N; i++) {
+				int temp = Math.abs(-i + (N-1)/2);
+				for(int x = temp; x < N - temp; x++) {
+					 sum += board[i][x];
+				}
+			}
+			System.out.printf("#%d %d\n",t,sum);
+		}
+	}
+	
 }
