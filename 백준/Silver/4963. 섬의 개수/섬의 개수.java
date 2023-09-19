19ce3e1f7eae459e3cceb9dcd2a539f52c1b046e
Yooooodong <animus0504@naver.com>
2023-09-19 17:26:50 +0900

    [Silver II] Title: 섬의 개수, Time: 312 ms, Memory: 31424 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,80 +1,83 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-	
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static int INF = Integer.MAX_VALUE;
-	
-	static int[] dx = {0,1,0,-1,-1,1,1,-1};
-	static int[] dy = {-1,0,1,0,-1,-1,1,1};
+	static int W, H; // 너비, 높이
+	static int[][] map; // 2차원 배열
+	static boolean[][] visited;
 
-	public static class Pos{
-		int x;
-		int y;
-		
-		public Pos() {}
-		public Pos(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
-	
-	public static void main(String[] args) throws IOException{
-		
-		while(true) {
-			st = new StringTokenizer(br.readLine());
-			int w = Integer.parseInt(st.nextToken());
-			int h = Integer.parseInt(st.nextToken());
-			
-			if(w == 0 && h == 0)break;
-			
-			int[][] board = new int[h][w];
-			int[][] visited = new int[h][w];
-			ArrayDeque<Pos> dq = new ArrayDeque<>();
-			
-			int count = 1;
-			
-			for(int i = 0; i < h; i++) {
-				board[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-			}
-			
-			for(int y=0; y < h; y++) {
-				for(int x = 0 ;  x < w; x++) {
-					if(visited[y][x] != 0 || board[y][x] == 0)continue;
-					
-					dq.add(new Pos(x,y));
-					
-					while(!dq.isEmpty()) {
-						Pos cur = dq.poll();
-						
-						if(visited[cur.y][cur.x] != 0)continue;
-						visited[cur.y][cur.x] = count;
-						
-						for(int i = 0 ; i < 8; i++) {
-							int mx = dx[i] + cur.x;
-							int my = dy[i] + cur.y;
-							
-							if(mx >= w || mx < 0 || my >= h || my < 0 || visited[my][mx] != 0 || board[my][mx] == 0)continue;
-							
-							dq.add(new Pos(mx,my));
-						}
-					}
-					
-					count++;
+
+	// 8방 탐색
+	static int[] dr = { -1, 1, 0, 0, 1, 1, -1, -1 };
+	static int[] dc = { 0, 0, -1, 1, -1, 1, -1, 1 };
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			W = sc.nextInt();
+			H = sc.nextInt();
+
+			if (W == 0 && H == 0)
+				break;
+
+			map = new int[H][W];
+			visited = new boolean[H][W]; 
+
+			// 2차원 배열 입력 받기
+			for (int r = 0; r < H; r++) {
+				for (int c = 0; c < W; c++) {
+					map[r][c] = sc.nextInt();
+
 				}
 			}
-			
-//			for(int i = 0; i < h ; i++)System.out.println(Arrays.toString(visited[i]));
-			System.out.println(count-1);
+
+			int cnt = 0; // 섬의 개수
+
+			// 2차원 배열을 행 우선 순회하면서
+			// 방문하지 않은 1을 만난 경우.
+			// 그 (r, c)부터 DFS탐색을 시작
+			for (int r = 0; r < H; r++) {
+				for (int c = 0; c < W; c++) {
+					if (map[r][c] == 1 && !visited[r][c]) {
+						visited[r][c] = true; // 시작점을 방문처리 후
+						dfs(r, c); // 시작점부터 탐색 시작					
+						cnt++; // 새로운 그래프를 발견했으므로.
+					}
+				}
+			}
+            
+			System.out.println(cnt);
+			cnt = 0; 
 		}
 		
-		
-		
 	}
-	
-	
-}
 
+	private static void dfs(int r, int c) {
+		// 현재 정점 (r, c)
+		// 이웃한 정점 중에서 아직 방문하지 않은 곳이 있다면
+		// 그 정점으로 탐색을 계속 해 나가면 됨(재귀)
+		// 재귀 -> 스택 -> dfs
+
+		// 이웃한 정점 : 후보 8개 -> 2차원 배열 범위 안 -> 1인 곳
+		// & 방문하지 않은 경우
+
+
+		for (int d = 0; d < 8; d++) {
+			int nr = r + dr[d];
+			int nc = c + dc[d];
+			if (nr >= 0 && nr < H && nc >= 0 && nc < W && map[nr][nc] == 1 && !visited[nr][nc]) {
+				visited[nr][nc] = true;
+				dfs(nr, nc); // 시작점부터 탐색 시작
+
+			}
+		}
+		
+
+
+	}
+
+	
+
+	
+
+}
